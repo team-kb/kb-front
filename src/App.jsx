cd9c29ecd703e94e1698257193246430f2173e2e
Rei <ironpopcorn@gmail.com>
2018-11-26 00:50:42 -0300

    Add calendar
    -Add more eslint configurations and plugins for react/jsx.
    -Add a basic calendar component.
    -Add key props for elements inside loops.
    -Add prop-type for typechecking props.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Calendario from './components/calendario';
 
 class App extends React.Component {
   state = {
     userType: 0,
     routes: [
       {
+        id: 'home',
         path: '/',
         exact: true,
         name: 'Home',
         component: () => <h1>Welcome</h1>,
       },
       {
+        id: 'agendarCita',
         path: '/book',
         name: 'Agendar cita',
         component: () => <h1>Agendar cita</h1>,
       },
       {
+        id: 'agregarVet',
         path: '/vet',
         name: 'Agregar Veterinario',
         component: () => <h1>Agregar veterinario</h1>,
@@ -38,6 +43,7 @@ class App extends React.Component {
   }
 }
 
+
 const Root = props => (
   <div style={{
     display: 'flex',
@@ -49,7 +55,7 @@ const Sidebar = props => (
     <p>Menú {(props.userType === 1 ? 'Paciente' : 'Clínica')}</p>
     <ul>
       {props.routes.map(route => (
-        <li>
+        <li key={`sidebar-${route.id}`}>
           <Link to={route.path}>{route.name}</Link>
         </li>
       ))}
@@ -57,16 +63,53 @@ const Sidebar = props => (
   </div>
 );
 
-const Main = props => (
-  <div style={{ flex: 1 }}>
-    {props.routes.map(route => (
-        <Route
-          path={route.path}
-          exact={route.exact}
-          component={route.component}
-        />
-    ))}
-  </div>
-);
+Sidebar.propTypes = {
+  userType: PropTypes.number.isRequired,
+  routes: PropTypes.array.isRequired,
+};
+
+class Main extends React.Component {
+  constructor(prop) {
+    super(prop);
+    this.state = {
+      month: 10,
+      year: 2018,
+    };
+    this.onMonthChanged = this.onMonthChanged.bind(this);
+    this.onYearChanged = this.onYearChanged.bind(this);
+  }
+
+  onMonthChanged(e) {
+    this.setState({ month: parseInt(e.target.value, 0) });
+  }
+
+  onYearChanged(e) {
+    this.setState({ year: parseInt(e.target.value, 0) });
+  }
+
+  render() {
+    const { month, year } = this.state;
+
+    return (
+      <div style={{ flex: 1 }}>
+        {this.props.routes.map(route => (
+            <Route
+              key={`main-${route.id}`}
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />
+        ))}
+        <input type="number" min={0} max={11} value={month} onChange={this.onMonthChanged} />
+        <input type="number" min={2018} max={2020} value={year} onChange={this.onYearChanged} />
+        <Calendario month={month} year={year} />
+      </div>
+    );
+  }
+}
+
+Main.propTypes = {
+  routes: PropTypes.array.isRequired,
+};
 
 export default App;
---
+++ src/components/calendario.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './calendario.scss';
+
+
+class Calendario extends React.Component {
+  days = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'];
+
+  months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+
+  renderHeaders() {
+    return this.days.map(day => <div key={`weekday-${day}`} className='header'>{day}</div>);
+  }
+
+  renderCells() {
+    const { year, month } = this.props;
+    const initialDate = new Date(year, month);
+    const finalDate = new Date(initialDate.getFullYear(), initialDate.getMonth() + 1, 0);
+    const cells = [];
+
+    for (let i = 0; i < initialDate.getDay(); i++) {
+      cells.push(<div key={`empty-${i}`}></div>);
+    }
+    for (let i = 1; i <= finalDate.getDate(); i++) {
+      cells.push(<div key={`day-${i}`}>{i}</div>);
+    }
+    return cells;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Mes {this.months[this.props.month]}</h1>
+        <div className='calendario'>
+          {this.renderHeaders()}
+          {this.renderCells()}
+        </div>
+      </div>
+    );
+  }
+}
+
+Calendario.propTypes = {
+  month: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+};
+
+export default Calendario;
